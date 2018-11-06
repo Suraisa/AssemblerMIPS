8b8508136c54427f8dff669837184915f47d4a17
Raphaël <raphael.gcardoso@gmail.com>
2018-11-06 10:21:13 +0100

    Add hashTable test to fix

--- test/testHashTable.c
+++ test/testHashTable.c
@@ -7,53 +7,25 @@ int main()
     if (!hashTable)
       return -1;
 
-    char* a = "test";
-    char* b = "secondtest";
-    char* c = "thirdtest";
-    char* d = "truc";
-    char* e = "truc";
+    char* stringTable[5] = {"test", "secondtest", "thirdtest", "truc", "truc"};
 
-    int ia, ib, ic, id, ie;
-    ia=Hash(a);
-    printf("Index of %s : %d \n",a,ia);
-    ib=Hash(b);
-    printf("Index of %s : %d \n",b,ib);
-    ic=Hash(c);
-    printf("Index of %s : %d \n",c,ic);
-    id=Hash(d);
-    printf("Index of %s : %d \n",d,id);
-    ie=Hash(e);
-    printf("Index of %s : %d \n",e,ie);
-
+    for (int indexString=0; indexString<5;indexString++){
+      int i;
+      i=Hash(stringTable[indexString]);
+      printf("Index of %s : %d \n",stringTable[indexString],i);
+    }
 
     LIST** ptr_hash=&hashTable;
-    char** ptr_a=&a;
-    if(!AddHashTable(ptr_hash,ptr_a))
-    {
-        printf("String already present");
+    for (int indexString=0; indexString<5;indexString++){
+      char** ptrString=&stringTable[indexString];
+      if(!AddHashTable(ptr_hash,ptrString))
+      {
+          printf("\nString %s : already present\n\n",stringTable[indexString]);
+      }
     }
-    char** ptr_b=&b;
-    if(!AddHashTable(ptr_hash,ptr_b))
-    {
-        printf("String already present");
-    }
-    char** ptr_c=&c;
-    if(!AddHashTable(ptr_hash,ptr_c))
-    {
-        printf("String already present");
-    }
-    char** ptr_d=&d;
-    if(!AddHashTable(ptr_hash,ptr_d))
-    {
-        printf("String already present");
-    }
-    char** ptr_e=&e;
-    if(!AddHashTable(ptr_hash,ptr_e))
-    {
-        printf("String already present");
-    }
+    
     DisplayHashTable(hashTable);
-
+    printf("\n");
     ErasedHashTable(ptr_hash);
     DisplayHashTable(hashTable);
     return 0;
