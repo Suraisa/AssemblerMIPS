ba2a568a6458d61005f6bdfbf5adfffbd1a1766d
Raphael <raphael.gcardoso@gmail.com>
2018-12-06 00:01:18 +0100

    miam_sujet.s

--- files/miam_sujet.s
+++ files/miam_sujet.s
@@ -2,4 +2,24 @@
 # allons au ru
 .set noreorder
 .text
-    J test
+    Lw $t0 , lunchtime
+    LW $6, -200($7)
+    ADDI $t1,$zero,8
+boucle:
+    BEQ $t0 , $t1 , byebye
+    NOP
+    addi $t1 , $t1 , 1
+    J boucle
+    NOP
+    BLT $1,$2,byebye
+byebye:
+    JAL viteviteauru
+
+.data
+lunchtime: 
+    .word 12
+    .word menu
+    .asciiz "ils : \"au ru!\""
+.bss 
+menu:
+    .space 24
